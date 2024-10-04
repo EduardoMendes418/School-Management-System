da61ef8d4cae42c22a3eb333997da1d75f8f3fd5
eduardo <eduardomendes418@gmail.com>
2024-10-04 18:32:25 -0300

    [SMS-24] feat: create BigCalendar and  add color

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ First, run the development server:
 ```bash
 npm run dev
 # or
-yarn dev
+yarn run dev
 # or
 pnpm dev
 # or
@@ -17,8 +17,6 @@ bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
 ## Learn More
 
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,16 +1,16 @@
 import Announcements from "@/components/announcements/announcements";
+import BigCalendar from "@/components/bigCalender/bigCalender";
 import EventCalendar from "@/components/eventCalendar/eventCalendar";
 
 const StudentPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
-      {/*Left*/}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-            <h1 className="text-xl font-semibold">Schedule(4A)</h1>
+          <h1 className="text-xl font-semibold">Schedule(4A)</h1>
+          <BigCalendar />
         </div>
       </div>
-      {/*Right*/}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
         <EventCalendar />
         <Announcements />
