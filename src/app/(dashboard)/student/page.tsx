01f710dbec2ece3e410f8ff680782ef0b4f09300
eduardo <eduardomendes418@gmail.com>
2024-10-04 13:57:05 -0300

    [SMS-22] feat:  create studentPage

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,22 @@
-const StudentPage = () => {
-    return (
-        <div className="">StudentPage</div>
-    )
-}
+import Announcements from "@/components/announcements/announcements";
+import EventCalendar from "@/components/eventCalendar/eventCalendar";
 
-export default StudentPage
+const StudentPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/*Left*/}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+            <h1 className="text-xl font-semibold">Schedule(4A)</h1>
+        </div>
+      </div>
+      {/*Right*/}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default StudentPage;
