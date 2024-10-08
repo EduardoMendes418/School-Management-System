87b6ded4c70ac2af697b8dbe9390c30629d4e570
eduardo <eduardomendes418@gmail.com>
2024-10-08 15:19:01 -0300

    [SMS-38] feat:  change Table and TeacherListPage

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,9 +1,11 @@
 import Pagination from "@/components/pagination/pagination";
 import Table from "@/components/table/table";
 import TableSearch from "@/components/tableSearch/tableSearch";
+import { role, teachersData } from "@/lib/data";
 import Image from "next/image";
+import Link from "next/link";
 
-type Teacher = [
+type Teacher = {
   id: number,
   teacherId: string,
   name: string,
@@ -11,9 +13,9 @@ type Teacher = [
   photo: string,
   phone: string,
   subjects: string[],
-  classe: string[],
+  classes: string[],
   addres: string
-];
+}
 
 const columns = [
   {
@@ -52,10 +54,40 @@ const columns = [
 ];
 
 const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => {};
+
+  const renderRow = (item: Teacher) => (
+    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-slate-100">
+      <td className="flex items-center gap-4 p-4">
+        <Image src={item.photo} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover" />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.subjects.join(',')}</td>
+      <td className="hidden md:table-cell">{item.classes.join(',')}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.addres}</td>
+      <td>
+        <div className=" flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-gray-100">
+              <Image src="/img/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full  bg-gray-100">
+              <Image src="/img/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  )
 
   return (
-    <div className="bg-white rounded-md flex-1 m-4 mt-0 p-4 ">
+    <div className="bg-white p-4  rounded-md flex-1 m-4 mt-0 ">
       <div className="flex items-center justify-between ">
         <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
         <div className=" flex flex-col md:flex-row items-center w-full md:w-auto gap-4">
@@ -73,7 +105,7 @@ const TeacherListPage = () => {
           </div>
         </div>
       </div>
-      <Table columns={columns} />
+      <Table columns={columns} renderRow={renderRow} data={teachersData} />
       <Pagination />
     </div>
   );
