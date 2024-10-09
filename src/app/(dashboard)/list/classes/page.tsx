b155102fd28e5aa8f4ecbaf885278a07f0092b61
eduardo <eduardomendes418@gmail.com>
2024-10-09 13:47:45 -0300

    [SMS-47] feat: add new img edit pages

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -53,7 +53,7 @@ const ClasseListPage = () => {
         <div className=" flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-gray-100">
-              <Image src="/img/view.png" alt="" width={16} height={16} />
+              <Image src="/img/edit2.png" alt="" width={16} height={16} />
             </button>
           </Link>
           {role === "admin" && (
