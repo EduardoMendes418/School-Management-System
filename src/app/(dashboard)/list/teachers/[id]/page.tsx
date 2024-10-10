551b6273ce8502c4d3ace45285b85dc1be3767e4
eduardo <eduardomendes418@gmail.com>
2024-10-10 14:45:02 -0300

    [SMS-61] feat:  add Announcements singleTeacher page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from "@/components/announcements/announcements";
 import BigCalendar from "@/components/bigCalender/bigCalender";
 import Image from "next/image";
+import Link from "next/link";
 
 const SingleTeacherPage = () => {
   return (
@@ -97,11 +99,33 @@ const SingleTeacherPage = () => {
           </div>
         </div>
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-            <h1>Teacher&apos;s Schedule</h1>
-            <BigCalendar/>
+          <h1>Teacher&apos;s Schedule</h1>
+          <BigCalendar />
         </div>
       </div>
-      <div className="w-full xl:w-1/3">r</div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-green-50" href="/">
+              Teacher&apos;s Classes
+            </Link>
+            <Link className="p-3 rounded-md bg-blue-50" href="/">
+              Teacher&apos;s Students
+            </Link>
+            <Link className="p-3 rounded-md bg-yellow-50" href="/">
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-green-50" href="/">
+              Teacher&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-blue-50" href="/">
+              Teacher&apos;s Assignments
+            </Link>
+          </div>
+        </div>
+        <Announcements />
+      </div>
     </div>
   );
 };
